966ebdf07301de8d4f91c0c52aac924c21d00eb2
nina <ninadhoraa@gmail.com>
2020-06-13 11:51:01 -0300

    hello world

---
+++ build/ros_teste_tutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nina/catkin_ws/src/ros_teste_tutorial
+# Build directory: /home/nina/catkin_ws/build/ros_teste_tutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
