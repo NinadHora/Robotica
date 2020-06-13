1ad00168367cdec177282294d5c479d50a660151
nina <ninadhoraa@gmail.com>
2020-06-13 11:58:13 -0300

    teste

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/nina/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/nina/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/nina/catkin_ws/devel;/opt/ros/melodic' != '' else []
